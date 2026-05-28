915d147f3a6feff7065a7c7e3fd9fa44775b6859
B_Drozd <bogdandrozd99@gmail.com>
2026-05-28 18:53:58 +0300

    add lesson4.2

---
+++ lesson4.2.py
@@ -0,0 +1,13 @@
+n = [2,3,4,5,0,8,0,6]
+if len(n) == 0:
+    r = 0
+else:
+    s = 0
+    for i in range(len(n)):
+        if i % 2 == 0:
+            s = s + n[i]
+    l= len(n) - 1
+    e = n[l]
+    r = s * e
+print(r)
+
