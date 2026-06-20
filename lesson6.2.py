a02e9554d2b23b73d831dbf5c41f73d32d51dbeb
B_Drozd <bogdandrozd99@gmail.com>
2026-06-20 17:48:50 +0300

    add lesson6.2

--- lesson6.2.py
+++ lesson6.2.py
@@ -9,17 +9,17 @@ m = se // 60
 sec = se % 60
 
 if 11 <= d % 100 <= 14:
-    day = "днів"
+    y = "днів"
 elif d % 10 == 1:
-    day = "день"
+    y = "день"
 elif d % 10 == 2 or d % 10 == 3 or d % 10 == 4:
-    day = "дні"
+    y = "дні"
 else:
-    day = "днів"
+    y = "днів"
 
 hours = str(h).zfill(2)
 minutes = str(m).zfill(2)
 seconds= str(sec).zfill(2)
 
-r = str(d) + " " + day + ", " + hours + ":" + minutes + ":" + seconds
+r = str(d) + " " + y + ", " + hours + ":" + minutes + ":" + seconds
 print(r)
