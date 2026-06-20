41f913269ac80e422ea945c6f6a2db33d9ddf629
B_Drozd <bogdandrozd99@gmail.com>
2026-06-20 17:46:38 +0300

    add lesson6.2

---
+++ lesson6.2.py
@@ -0,0 +1,25 @@
+
+s = int(input("Введіть кількість секунд: "))
+
+d = s // 86400
+se = s % 86400
+h = se // 3600
+se = se % 3600
+m = se // 60
+sec = se % 60
+
+if 11 <= d % 100 <= 14:
+    day = "днів"
+elif d % 10 == 1:
+    day = "день"
+elif d % 10 == 2 or d % 10 == 3 or d % 10 == 4:
+    day = "дні"
+else:
+    day = "днів"
+
+hours = str(h).zfill(2)
+minutes = str(m).zfill(2)
+seconds= str(sec).zfill(2)
+
+r = str(d) + " " + day + ", " + hours + ":" + minutes + ":" + seconds
+print(r)
