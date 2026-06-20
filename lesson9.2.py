884bbba73c1deae1bd448f3ebadcc76f174da184
B_Drozd <bogdandrozd99@gmail.com>
2026-06-20 19:20:54 +0300

    add lesson9.2

---
+++ lesson9.2.py
@@ -0,0 +1,13 @@
+def difference(*a):
+
+    if not a:
+        return 0
+    r = max(a) - min(a)
+    return round(r, 2)
+
+assert difference(1, 2, 3) == 2, "Test1"
+assert difference(5, -5) == 10, "Test2"
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, "Test3"
+assert difference() == 0, "Test4"
+
+print("OK")
